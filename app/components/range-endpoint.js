af2649d0b5abd96ce9b94dabf5bf36a7a852ac04
hbaughman <hbaughman741@gmail.com>
2015-04-30 13:50:12 -0400

    Create action to increment endpointTime

--- app/components/range-endpoint.js
+++ app/components/range-endpoint.js
@@ -4,10 +4,20 @@ export default Ember.Component.extend({
   endpointTime: 0,
 
   actions: {
-    "setEndpointToCurrentTime": function() {
-      debugger;
+    setEndpointToCurrentTime: function() {
       var currentTime = this.get('currentTime');
       this.set('endpointTime', currentTime);
+    },
+
+    incrementEndpoint: function(incrementBy) {
+      var newEndpoint = this.get('endpointTime') + incrementBy;
+
+      // All legal currentTime must be bounded by 0 and the duration.
+      newEndpoint = newEndpoint < 0 ? 0 : newEndpoint;
+      var duration = this.get('duration');
+      newEndpoint = newEndpoint > duration ? duration : newEndpoint;
+
+      this.set('endpointTime', newEndpoint);
     }
   }
 });
