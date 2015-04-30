6479d362dc8a4393d354b873c30306f77cf51ae2
hbaughman <hbaughman741@gmail.com>
2015-04-30 18:45:40 -0400

    Add end rangeEndpoint

--- app/components/range-endpoint.js
+++ app/components/range-endpoint.js
@@ -4,11 +4,6 @@ export default Ember.Component.extend({
   endpointTime: 0,
 
   actions: {
-    setEndpointToCurrentTime: function() {
-      var currentTime = this.get('currentTime');
-      this.set('endpointTime', currentTime);
-    },
-
     incrementEndpoint: function(incrementBy) {
       var newEndpoint = this.get('endpointTime') + incrementBy;
 
@@ -18,6 +13,17 @@ export default Ember.Component.extend({
       newEndpoint = newEndpoint > duration ? duration : newEndpoint;
 
       this.set('endpointTime', newEndpoint);
+    }, 
+
+    setEndpointToCurrentTime: function() {
+      var currentTime = this.get('currentTime');
+      this.set('endpointTime', currentTime);
+    },
+
+    setCurrentTimeToEndpoint: function() {
+      var endpointTime = this.get('endpointTime');
+      debugger;
+      this.set('currentTime', endpointTime);
     }
   }
 });
